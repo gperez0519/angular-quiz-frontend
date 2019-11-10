965d0b8af6b819b86c172b7ce5697b409ce56de5
Gabriel Perez <gperez0519@gmail.com>
2019-11-10 15:30:24 -0500

    Added functionality to get questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card'; 
+import { FormsModule } from '@angular/forms' 
+import { ApiService } from './api.service'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component'
+import { QuestionsComponent } from './questions.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, QuestionComponent, QuestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatListModule,
+    MatCardModule
+    
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
