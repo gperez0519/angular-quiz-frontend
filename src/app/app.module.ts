9cdb1fb56e9ab5ef264900049b12c129a44e2806
Gabriel Perez <gperez0519@gmail.com>
2019-11-15 18:14:33 -0500

    Added nav bar and quiz components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,47 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card'; 
-import { FormsModule } from '@angular/forms' 
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 import { ApiService } from './api.service'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component'
 import { QuestionsComponent } from './questions.component'
+import { HomeComponent } from './home.component'
+import { NavComponent } from './nav.component'
+import { QuizComponent } from './quiz.component'
+
+const routes = [
+  { path: '', component: HomeComponent},
+  { path: 'question', component: QuestionComponent},
+  { path: 'questions', component: QuestionsComponent},
+  { path: 'quiz', component: QuizComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent, QuestionComponent, QuestionsComponent
+    AppComponent,
+    QuestionComponent,
+    QuestionsComponent,
+    HomeComponent,
+    NavComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatToolbarModule,
     MatInputModule,
     MatListModule,
     MatCardModule
